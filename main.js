0d6ab9d6ebfeb081a55264dba523e9f566e24f24
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-08-28 22:30:27 +0200

    prepering for OOP

---
+++ main.js
@@ -0,0 +1,16 @@
+const btnReset = document.querySelector("[data-function='reset']")
+const btnStart = document.querySelector("[data-function='start']")
+const btnLap = document.querySelector("[data-function='lap']")
+
+class List {
+
+}
+
+class Panel {
+    constructor(btnReset, btnStart, btnLap) {
+
+    }
+}
+
+
+const panel = new Panel(btnReset, btnStart, btnLap);
